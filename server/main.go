1d48b206084b858399f2aacd58b7485103617535
snowmerak <snowmerak@outlook.com>
2022-05-19 19:37:43 +0900

    Update main.go

--- server/main.go
+++ server/main.go
@@ -4,10 +4,14 @@ import (
 	"net/http"
 
 	"github.com/snowmerak/workspace-demo/router/index"
+	"github.com/snowmerak/workspace-demo/router/person"
+	"github.com/snowmerak/workspace-demo/router/weather"
 )
 
 func main() {
 	http.HandleFunc("/", index.Index)
+	http.HandleFunc("/person", person.Index)
+	http.HandleFunc("/weather", weather.Index)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
